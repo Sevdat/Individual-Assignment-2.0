0a57e0d418d1b92f57e52e0684839c492d8d0120
Sevdat <sevdat1999@gmail.com>
2021-12-24 07:14:02 +0100

    fixes

--- src/Experiment.kt
+++ src/Experiment.kt
@@ -1,52 +1,51 @@
 import java.io.File
 
 fun asf1(textdoc:File):MutableMap<String,Int> {
-    val fileList =  textdoc.readLines()
-    println("fileList-----------------= $fileList")
 
 //вынимает все, кроме алфавитов и все, кроме цифр и удаляет сгенерированный пробел
-    var numberList = mutableListOf<String>()
-    var nameList = mutableListOf<String>()
-    for (i in fileList) {
-        numberList = (numberList + i.replace(Regex("[^0-9]"), " ").trim()) as MutableList<String>
-        nameList = (nameList + i.replace(Regex("[ 0-9]"), " ").trim()) as MutableList<String>
-    }
-    println("nameList----------------= $nameList")
-    println("numberList--------------= $numberList")
 
-    var vs = 0
-    var kes = mutableListOf<List<String>>()
-    for (i in numberList) {
-        while (vs != numberList.size ) {
-            kes += numberList[vs].split(" ").filter { p -> p != "" }
-            vs += 1
-    }
-    }
-    println("kes---------------------= $kes")
-
-fun kek(x: Int, y:Int): Pair<Int,Int> {
-        if(x != y) {
-            var list = kes[x][y]
-            var list1 = kes[y][x]
-            return Pair(list.toInt(), list1.toInt())
-        }
-    return Pair(-1,-1)
-}
-
-//from coordinate to adding score
+//using the line size,
     var d = -1
-    var f = -1
-    var list3 = mutableListOf<Int>()
     var list4 = mutableListOf<Int>()
-    var point = mutableListOf<Int>()
     val teamScore = mutableMapOf<String, Int>()
-    while (d != kes.size - 1){
+
+    while (d != textdoc.readLines().size - 1){
+
+        var numberList = mutableListOf<String>()
+        var nameList = mutableListOf<String>()
+        var kes1 = mutableListOf<List<String>>()
+        var vs = 0
+        for (i in textdoc.readLines()) {
+            numberList = (numberList + i.replace(Regex("[^0-9]"), " ").trim()) as MutableList<String>
+            nameList = (nameList + i.replace(Regex("[ 0-9]"), " ").trim()) as MutableList<String>
+            while (vs != numberList.size) {
+                kes1 += numberList[vs].split(" ").filter { p -> p != "" }
+                vs += 1
+            }
+        }
+        println("nameList----------------= $nameList")
+        println("numberList--------------= $numberList")
+        println("kes1--------------------= $kes1")
+
+        fun kek(x: Int, y:Int): Pair<Int,Int> {
+            if(x != y) {
+                var list = kes1[x][y]
+                var list1 = kes1[y][x]
+                return Pair(list.toInt(), list1.toInt())
+            }
+            return Pair(-1,-1)
+        }
+
         d += 1
+
         var list2 = mutableListOf<Pair<Int,Int>>()
-        while(f != kes.size - 1) {
+        var f = -1
+        while(f != textdoc.readLines().size - 1) {
             f += 1
             list2 = (list2 + kek(d,f)) as MutableList<Pair<Int, Int>>
         }
+
+        var point = mutableListOf<Int>()
         for ((q, w) in list2.filter { i -> i != Pair(-1,-1)}) {
             when {
                 (q > w) -> point.add(3)
@@ -54,14 +53,14 @@ fun kek(x: Int, y:Int): Pair<Int,Int> {
                 else -> point.add(1)
             }
         }
-        list3 = listOf(point.sum()) as MutableList<Int>
+
+       var list3 = listOf(point.sum()) as MutableList<Int>
         list4 += list3
         list2.clear()
         point.clear()
-        f = -1
 
         teamScore[nameList[d]] = list4[d]
     }
-        println("teamScore---------------= $teamScore")
+    println("teamScore---------------= $teamScore")
     return teamScore
 }
