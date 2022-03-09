c7dfc1c1ae245ec0803253ca4f72d826ed2586ef
Sevdat <sevdat1999@gmail.com>
2022-03-09 12:54:54 +0100

    fixes

--- src/Exam2022productsKtTest.kt
+++ src/Exam2022productsKtTest.kt
@@ -2,21 +2,21 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
-internal class Exam2022_productsKtTest {
+internal class Exam2022productsKtTest {
 
     @Test
     fun foo() {
         kotlin.test.assertEquals(
             mutableListOf("яблоки"),
-            foo("src/Exam2022 products.txt","00???2 10")
+            foo("src/Exam2022products.txt","00???2 10")
         )
         kotlin.test.assertEquals(
             mutableListOf("яйца, достаточно, 1380 р"),
-            foo("src/Exam2022 products.txt","009724 15")
+            foo("src/Exam2022products.txt","009724 15")
         )
         kotlin.test.assertEquals(
             mutableListOf("яблоки", "молоко"),
-            foo("src/Exam2022 products.txt","* 35")
+            foo("src/Exam2022products.txt","* 35")
         )
     }
 }
